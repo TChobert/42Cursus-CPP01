7cca8e641c30b824fe92f2b1d7c76363e69d90b0
Thomas Chobert <tchobert@paul-f4Br4s7.clusters.42paris.fr>
2025-03-21 20:58:08 +0100

    exercice 01 - Makefile a faire

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 19:46:00 by tchobert          #+#    #+#             */
+/*   Updated: 2025/03/21 19:46:02 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private:
+
+	std::string	_name;
+
+	public:
+
+	Zombie(const std::string& name);
+	~Zombie();
+	void	announce(void);
+};
+
+void	randomChump(std::string name);
+Zombie*	newZombie(std::string name);
---
+++ ex00/newZombie.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 20:32:36 by tchobert          #+#    #+#             */
+/*   Updated: 2025/03/21 20:32:51 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie*	newZombie(std::string name)
+{
+	Zombie	*new_zombie;
+
+	new_zombie = new Zombie(name);
+	return	(new_zombie);
+}
