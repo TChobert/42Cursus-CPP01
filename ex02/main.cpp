51876dba4ed79551e024b8806591e6d09de44c7d
Thomas Chobert <tchobert@made-f0Cr10s6.clusters.42paris.fr>
2025-04-15 20:28:41 +0200

    exercice 02

---
+++ ex02/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 20:17:02 by tchobert          #+#    #+#             */
+/*   Updated: 2025/04/15 20:17:30 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+int	main(void)
+{
+	const std::string	string = "HI THIS IS BRAIN";
+	const std::string*	stringPTR = &string;
+	const std::string&	stringREF = string;
+
+	std::cout << "Memory address of string: " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+}
